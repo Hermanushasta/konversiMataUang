66d7f7d6a79abd45b1bc2798e27884992fd37be1
Hermanus Hasta Wicaksana <hasta.wicaksana@gmail.com>
2023-03-25 10:35:07 +0700

    base project tp03

--- app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
+++ app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
@@ -2,7 +2,9 @@ package org.d3if0043.konversimatauang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.widget.Toast
 import org.d3if0043.konversimatauang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun konvertMataUang() {
-        Log.d("MainActivity", "Tombol diklik")
+        val berat = binding.beratEditText.text.toString()
+        if (TextUtils.isEmpty(berat)) {
+            Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggi = binding.tinggiEditText.text.toString()
+        if (TextUtils.isEmpty(tinggi)) {
+            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val tinggiCm = tinggi.toFloat() / 100
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        if (selectedId == -1) {
+            Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val isMale = selectedId == R.id.priaRadioButton
+        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
+        val kategori = getKategori(bmi, isMale)
+        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
     }
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale) {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        } else {
+            when {
+                bmi < 18.5 -> R.string.kurus
+                bmi >= 25.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
+    }
+
 }
