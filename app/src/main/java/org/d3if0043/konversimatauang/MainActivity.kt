edb66b7a0b59eba89ccddc460079494f3f64baff
Hermanus Hasta Wicaksana <hasta.wicaksana@gmail.com>
2023-03-26 20:18:26 +0700

    dasar convert

--- app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
+++ app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
@@ -18,43 +18,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun konvertMataUang() {
-        val berat = binding.beratBadanInp.text.toString()
+        val berat = binding.masukanIDRInp.text.toString()
         if (TextUtils.isEmpty(berat)) {
             Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val tinggi = binding.tinggiBadanInp.text.toString()
-        if (TextUtils.isEmpty(tinggi)) {
-            Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val tinggiCm = tinggi.toFloat() / 100
-        val selectedId = binding.radioGroup.checkedRadioButtonId
-        if (selectedId == -1) {
-            Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val isMale = selectedId == R.id.priaRadioButton
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
+        val kursUSD = 15166
+        val bmi = berat.toFloat() / kursUSD
         binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
-        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-    }
-    private fun getKategori(bmi: Float, isMale: Boolean): String {
-        val stringRes = if (isMale) {
-            when {
-                bmi < 20.5 -> R.string.kurus
-                bmi >= 27.0 -> R.string.gemuk
-                else -> R.string.ideal
-            }
-        } else {
-            when {
-                bmi < 18.5 -> R.string.kurus
-                bmi >= 25.0 -> R.string.gemuk
-                else -> R.string.ideal
-            }
-        }
-        return getString(stringRes)
-    }
 
+    }
 }
