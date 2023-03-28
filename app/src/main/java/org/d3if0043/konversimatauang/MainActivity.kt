ea30bf527eb31703f4166721823ca7a974ca567d
Hermanus Hasta Wicaksana <hasta.wicaksana@gmail.com>
2023-03-28 13:52:56 +0700

    memperbaiki nama variabel dan menambahkan tema gelap

--- app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
+++ app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
@@ -3,7 +3,6 @@ package org.d3if0043.konversimatauang
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.widget.Toast
 import org.d3if0043.konversimatauang.databinding.ActivityMainBinding
 
@@ -18,14 +17,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun konvertMataUang() {
-        val berat = binding.masukanIDRInp.text.toString()
-        if (TextUtils.isEmpty(berat)) {
-            Toast.makeText(this, R.string.berat_invalid, Toast.LENGTH_LONG).show()
+        val nominal = binding.masukanIDRInp.text.toString()
+        if (TextUtils.isEmpty(nominal)) {
+            Toast.makeText(this, R.string.nominal_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val kursUSD = 15166
-        val bmi = berat.toFloat() / kursUSD
-        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        val selectRadioButton = binding.radioGroup.checkedRadioButtonId
+        if (selectRadioButton == -1) {
+            Toast.makeText(this, R.string.radioInvalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val isIDR = selectRadioButton == R.id.idrToUsdRadioButton
+        val kursUSD = 15166.0f
+
+        val nominalKonversi =getNominal(isIDR, nominal.toFloat(), kursUSD )
+        binding.hasilTextView.text = nominalKonversi
 
     }
+    private fun getNominal(isIDR: Boolean, nominal:Float, kursUSD:Float): String {
+
+        val hasil : Float =
+        if (isIDR) {
+            nominal / kursUSD
+        } else {
+            nominal * kursUSD
+        }
+        return hasil.toString()
+    }
+
 }
