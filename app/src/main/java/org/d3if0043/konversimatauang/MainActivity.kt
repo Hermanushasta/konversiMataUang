9187f4f52f6ab705d5619e6f2ae6eb93e62e384b
Hermanus Hasta Wicaksana <hasta.wicaksana@gmail.com>
2023-06-12 10:56:49 +0700

    task 6 done

--- app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
+++ app/src/main/java/org/d3if0043/konversimatauang/MainActivity.kt
@@ -1,21 +1,50 @@
 package org.d3if0043.konversimatauang
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import org.d3if0043.konversimatauang.util.MyTimer
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var myTimer: MyTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        myTimer = MyTimer()
+        Log.i("MainActivity", "onCreate Dipanggil")
+
         navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        myTimer.startTimer()
+        Log.i("MainActivity", "onStart dijalankan")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dijalankan")
+    }
+    override fun onPause() {
+        Log.i("MainActivity", "onPause dijalankan")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i("MainActivity", "onStop dijalankan")
+        myTimer.stopTimer()
+        super.onStop()
+    }
+    override fun onDestroy() {
+        Log.i("MainActivity", "onDestroy dijalankan")
+        super.onDestroy()
     }
 
     override fun onSupportNavigateUp(): Boolean {
